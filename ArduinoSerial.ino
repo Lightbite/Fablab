35237dffbcdddb8dd1a169fdaa83b751ac930890
Lightbite <cyberkanzler@gmail.com>
2018-05-18 16:18:37 +0200

    Add files via upload

---
+++ ArduinoSerial.ino
@@ -0,0 +1,24 @@
+
+int x = 1000 ;
+
+void setup() {
+  // start serial port at 9600 bps:
+
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+   pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+
+String temp = Serial.readString();
+x = temp.toInt();
+digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(x);   
+ digitalWrite(LED_BUILTIN, LOW); 
+ delay(x);    
+  }
+
+
